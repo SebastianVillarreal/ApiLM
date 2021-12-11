d3fe7a9b60258d6551449369b1619e3cee477c04
SebastianVillarreal <sebas_villalva13@hotmail.com>
2021-12-11 16:59:22 -0600

    permitir conexiones http dev

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace ApiLM
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiLM v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
